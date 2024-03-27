6f4ea7bd824695c170052f9ff3689fe4d8a470e3
tyoma <chertezhy.ru@gmail.com>
2024-03-27 14:24:58 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        double[] b = {1.57, 7.654, 9.986};
+        int[] c = new int[15];
+        for (int i = 0; i < 15; i++) {
+            c[i] = i * 2;
+        }
+        //task2
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(c));
+        //task3
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.print(a[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = b.length - 1; i >= 0; i--) {
+            System.out.print(b[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = c.length - 1; i >= 0; i--) {
+            System.out.print(c[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        //task4
+        for (int i = 0; i <= a.length-1; i++) {
+            int f = a[i];
+            if ((f % 2) != 0) {
+                f = f + 1;
+            }
+            System.out.print(f);
+            if (i < 2) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
