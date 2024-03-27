ab13ec6d53a9346433af069177a6cf32a5df0a5a
tyoma <chertezhy.ru@gmail.com>
2024-03-27 23:18:42 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -40,9 +40,10 @@ public class Main {
         System.out.println();
         //task4
         for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 != 0) a[i] = a[i] + 1;
+            if (a[i] % 2 != 0) {
+                a[i] = a[i] + 1;
+            }
         }
         System.out.println(Arrays.toString(a));
     }
-
 }
